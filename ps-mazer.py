be35ae88a54788bf8a433c7b9104ff8fa25dd7fe
Gary Algier <galgier52@gmail.com>
2024-05-14 14:55:29 -0400

    add clear button

--- ps-mazer.py
+++ ps-mazer.py
@@ -24,10 +24,13 @@ COLOR_DEAD = 'gray50'
 COLOR_SOLUTION = 'cyan'
 
 def draw_maze(ctx, width, height):
+    clear_maze(ctx, width, height)
+    draw_grid(ctx, width, height, COLOR_WALL)
+
+def clear_maze(ctx, width, height):
     ctx.clearRect(0, 0,
         (width + 1) * SPACING - 1,
         (height + 1) * SPACING - 1)
-    draw_grid(ctx, width, height, COLOR_WALL)
 
 def draw_grid(ctx, width, height, style):
     maxx = (width + 1) * SPACING - 1
@@ -51,10 +54,19 @@ def draw_horz(ctx, y, minx, maxx, style):
     ctx.strokeStyle = style
     ctx.stroke()
 
+def on_clear(*args):
+    canvas = document.getElementById("maze")
+    ctx = canvas.getContext("2d")
+    clear_maze(ctx, 10, 10)
+
 def on_generate(*args):
     canvas = document.getElementById("maze")
     ctx = canvas.getContext("2d")
     draw_maze(ctx, 10, 10)
+
+clear_proxy = create_proxy(on_clear)
+element = document.getElementById("clear_button")
+element.addEventListener("click", clear_proxy)
 
 generate_proxy = create_proxy(on_generate)
 element = document.getElementById("generate_button")
